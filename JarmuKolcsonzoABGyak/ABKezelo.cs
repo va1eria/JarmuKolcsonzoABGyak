509141cdc8ed8312cd1b45890892ffd11fba4d96
va1eria <valeria.varadi@outlook.com>
2021-12-11 14:19:27 +0100

    Kolcsonzo modositasa

--- JarmuKolcsonzoABGyak/ABKezelo.cs
+++ JarmuKolcsonzoABGyak/ABKezelo.cs
@@ -43,27 +43,32 @@ namespace JarmuKolcsonzoABGyak
         }
 
         // CRUD muvletek
+
+        static void TelepulesSzinkronizalasa(Cim beszur)
+        {
+            command.CommandText = "SELECT [IRSZ] FROM [Telepules] WHERE [IRSZ] = @irsz";
+            command.Parameters.AddWithValue("@irsz", beszur.Irsz.ToString());
+            bool voltIlyen = false;
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                voltIlyen = reader.HasRows;
+            }
+            if (!voltIlyen)
+            {
+                command.CommandText = "INSERT INTO [Telepules] VALUES (@irsz, @telep)";
+                command.Parameters.AddWithValue("@telep", beszur.Telepules);
+                command.ExecuteNonQuery();
+            }
+        }
         public static void KolcsonzoFelvitel(Kolcsonzo uj)
         {
             Csatlakozas();
 
-            // check hogy letezik-e a telepules, ha nem akkor letre kell hozni
-            command.Transaction = connection.BeginTransaction();
             try
             {
-                command.CommandText = "SELECT [IRSZ] FROM [Telepules] WHERE [IRSZ] = @irsz";
-                command.Parameters.AddWithValue("@irsz", uj.Cim.Irsz.ToString());
-                bool voltIlyen = false;
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    voltIlyen = reader.HasRows;
-                }
-                if (!voltIlyen)
-                {
-                    command.CommandText = "INSERT INTO [Telepules] VALUES (@irsz, @telep)";
-                    command.Parameters.AddWithValue("@telep", uj.Cim.Telepules);
-                    command.ExecuteNonQuery();
-                }
+                // check hogy letezik-e a telepules, ha nem akkor letre kell hozni
+                command.Transaction = connection.BeginTransaction();
+                TelepulesSzinkronizalasa(uj.Cim);
                 // kolcsonzo beszurasa
                 command.Parameters.Clear();
                 command.CommandText = "INSERT INTO [Kolcsonzo]([IRSZ], [Kozterulet], [KozteruletJellege], [Hazszam], [Megnevezes]) OUTPUT INSERTED.Id VALUES (@irsz, @kozt, @koztj, @hsz, @megn)";
@@ -73,14 +78,68 @@ namespace JarmuKolcsonzoABGyak
                 command.Parameters.AddWithValue("@hsz", uj.Cim.Hazszam);
                 command.Parameters.AddWithValue("@megn", uj.Megnevezes);
                 uj.Id = (int)command.ExecuteScalar();
+                command.Transaction.Commit();
             }
             catch (Exception ex)
             {
-                throw new ABKivetel("", ex);
+                try
+                {
+                    if (command.Transaction != null)
+                    {
+                        command.Transaction.Rollback();
+                    }
+
+                }
+                catch (Exception ex2)
+                {
+                    throw new ABKivetel("Vegzetes hiba az adatbazisban! Ertesitse a rendszergazdat!", ex2);
+                }
+                throw new ABKivetel("Sikertelen kolcsonzo felvitel!", ex);
+            }
+            finally
+            {
+                KapcsolatBontas();
             }
         }
 
-        public static void KolcsonzoModositas(Kolcsonzo modosit) { }
+        public static void KolcsonzoModositas(Kolcsonzo modosit)
+        {
+            Csatlakozas();
+            try
+            {
+                command.Transaction = connection.BeginTransaction();
+                TelepulesSzinkronizalasa(modosit.Cim);
+                command.Parameters.Clear();
+                command.CommandText = "UPDATE [Kolcsonzo] SET [Megnevezes] = @megn, [Kozterulet] = @kozt, [KozteruletJellege] = @koztj, [Hazszam] = @hsz, [IRSZ] = @irsz WHERE [Id] = @id";
+                command.Parameters.AddWithValue("@megn", modosit.Megnevezes);
+                command.Parameters.AddWithValue("@kozt", modosit.Cim.Kozterulet);
+                command.Parameters.AddWithValue("@koztj", (int)modosit.Cim.KozteruletJellege);
+                command.Parameters.AddWithValue("@hsz", modosit.Cim.Hazszam);
+                command.Parameters.AddWithValue("@irsz", modosit.Cim.Irsz.ToString());
+                command.ExecuteNonQuery();
+                command.Transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    if (command.Transaction != null)
+                    {
+                        command.Transaction.Rollback();
+                    }
+
+                }
+                catch (Exception ex2)
+                {
+                    throw new ABKivetel("Vegzetes hiba az adatbazisban! Ertesitse a rendszergazdat!", ex2);
+                }
+                throw new ABKivetel("Sikertelen kolcsonzo modositas!", ex);
+            }
+            finally
+            {
+                KapcsolatBontas();
+            }
+        }
 
         public static void KolcsonzoTorles(Kolcsonzo torol) { }
 
