41a101459aa36e158998ddc5cb1b50f80e2d3608
va1eria <valeria.varadi@outlook.com>
2022-01-15 16:05:56 +0100

    Forms

--- JarmuKolcsonzoABGyak/Form1.cs
+++ JarmuKolcsonzoABGyak/Form1.cs
@@ -16,5 +16,68 @@ namespace JarmuKolcsonzoABGyak
         {
             InitializeComponent();
         }
+
+        void LBFrissit()
+        {
+            try
+            {
+                int sIndex = listBox1.SelectedIndex;
+                listBox1.DataSource = null;
+                listBox1.DataSource = ABKezelo.TeljesFelolvasas();
+                if (sIndex < (listBox1.DataSource as List<Kolcsonzo>).Count)
+                {
+                    listBox1.SelectedIndex = sIndex;
+                }
+            }
+            catch (ABKivetel ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.DataSource = null;
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox2.DataSource = (listBox1.SelectedItem as Kolcsonzo).Jarmuvek;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KolcsonzoFrm frm=new KolcsonzoFrm();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                LBFrissit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex!=-1)
+            {
+                KolcsonzoFrm frm = new KolcsonzoFrm((Kolcsonzo)listBox1.SelectedItem);
+                if (frm.ShowDialog() == DialogResult.OK)
+                {
+                    LBFrissit();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedIndex!=-1)
+                {
+                    ABKezelo.KolcsonzoTorles((Kolcsonzo)listBox1.SelectedItem);
+                    LBFrissit();
+                }
+            }
+            catch (ABKivetel ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
---
+++ JarmuKolcsonzoABGyak/KolcsonzoFrm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JarmuKolcsonzoABGyak
+{
+    public partial class KolcsonzoFrm : Form
+    {
+        Kolcsonzo kolcsonzo;
+        internal Kolcsonzo Kolcsonzo { get => kolcsonzo; }
+        public KolcsonzoFrm()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(KozteruletJelleg));
+        }
+
+        internal KolcsonzoFrm(Kolcsonzo modosit)
+        {
+            kolcsonzo = modosit;
+            textBox1.Text = kolcsonzo.Megnevezes;
+            numericUpDown1.Value = kolcsonzo.Cim.Irsz;
+            textBox2.Text = kolcsonzo.Cim.Telepules;
+            textBox3.Text = kolcsonzo.Cim.Kozterulet;
+            comboBox1.SelectedItem = kolcsonzo.Cim.KozteruletJellege;
+            textBox4.Text = kolcsonzo.Cim.Hazszam;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (kolcsonzo == null)
+                {
+                    kolcsonzo = new Kolcsonzo(
+                        textBox1.Text,
+                        new Cim(
+                            textBox2.Text,
+                            textBox3.Text,
+                            textBox4.Text,
+                            (short)numericUpDown1.Value,
+                            (KozteruletJelleg)comboBox1.SelectedItem
+                            ));
+                    ABKezelo.KolcsonzoFelvitel(kolcsonzo);
+                }
+                else
+                {
+                    kolcsonzo.Megnevezes = textBox1.Text;
+                    kolcsonzo.Cim = new Cim(
+                            textBox2.Text,
+                            textBox3.Text,
+                            textBox4.Text,
+                            (short)numericUpDown1.Value,
+                            (KozteruletJelleg)comboBox1.SelectedItem
+                            );
+                    ABKezelo.KolcsonzoModositas(kolcsonzo);
+                }
+            }
+            catch (ABKivetel ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DialogResult = DialogResult.None;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Figyelem!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                DialogResult = DialogResult.None;
+            }
+        }
+    }
+}
